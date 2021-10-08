1d48589b224985fe1628352c0909ed1c970b4cbe
ema <ema@guidewire.com>
2021-10-08 16:47:26 -0400

    Web app with meal pictures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import RecipeCard from './components/RecipeCard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meal: ""
+    }
+  }
+
+  componentDidMount() {
+    const URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=b`
+    axios.get(URL)
+    .then(response => {
+      const meal = response.data.meals;
+      if (typeof meal === 'object') {
+        this.setState({ meal });
+      }
+    })
+    .catch(error => console.log(error));
+  }
+
+  render() {
+    var data = this.state.meal;
+    return (
+      <AppContainer className="App">
+        {data.length > 0 && <RecipeCard meals={data} />}
+      </AppContainer>
+    );
+  }
 }
 
-export default App;
+const AppContainer = styled.div`
+  background: #fff;
+  padding: 2em;
+`;
